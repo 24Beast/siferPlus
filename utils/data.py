fa0c4609d22342644983eba9a93fd36f4364c069
Bhanu Tokas <40471796+24Beast@users.noreply.github.com>
2024-04-23 10:09:20 -0700

    Added Download Support

--- utils/data.py
+++ utils/data.py
@@ -5,19 +5,36 @@ from torch.utils.data import Dataset
 from torchvision.transforms import transforms
 from torchvision.datasets import CelebA
 
+
 # Class Definition
 class CelebATarget(Dataset):
-    
-    def __init__(self, folder_path, transform = transforms.PILToTensor(), target_transform = None, split = "train"):
-        self.data = CelebA(root = folder_path, target_type = "attr", transform = transform, target_transform = target_transform, split = split)
-        self.hair_pos = [8,9,11,17]
+    def __init__(
+        self,
+        folder_path,
+        transform=transforms.PILToTensor(),
+        target_transform=None,
+        split="train",
+    ):
+        download=False
+        if(os.path.isdir(os.path.join(folder_path,"celeba/"))):
+           download = True
+        self.data = CelebA(
+            root=folder_path,
+            target_type="attr",
+            transform=transform,
+            target_transform=target_transform,
+            split=split,
+            download = download
+        )
+        self.hair_pos = [8, 9, 11, 17]
         self.gender_pos = 20
-        
+
     def __getitem__(self, index):
-        img, label = self.data[index].to(torch.float)
-        y = label[self.hair_pos]
-        target = label[self.gender_pos]
+        img, label = self.data[index]
+        img = img.to(torch.float)
+        y = label[self.hair_pos].to(torch.float)
+        target = label[self.gender_pos].to(torch.float)
         return img, y, target
-    
+
     def __len__(self):
         return len(self.data)
